09d98da128782275b80107894c0571fe4ba0f720
Wongsatorn Prompim <wongsatorn211@gmail.com>
2026-01-12 01:06:11 +0700

    change user import

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const User = require("../models/User");
+const User = require("../models/user");
 
 const router = express.Router();
 
--- routes/game.js
+++ routes/game.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const User = require("../models/User");
+const User = require("../models/user");
 const auth = require("../middleware/auth");
 
 const router = express.Router();
