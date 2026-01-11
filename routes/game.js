7f30a004545085806e1df6f5fa09771939e706c1
Wongsatorn Prompim <wongsatorn211@gmail.com>
2026-01-12 01:00:37 +0700

    change import

--- routes/game.js
+++ routes/game.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const User = require("../models/user.js");
+const User = require("../models/User");
 const auth = require("../middleware/auth");
 
 const router = express.Router();
